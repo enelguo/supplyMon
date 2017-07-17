f972db359af99580807c68851976096ecfba528a
yong guo <51335281@qq.com>
2017-07-17 15:51:31 +0000

    add static pages

--- app/Http/Controllers/staticPagesController.php
+++ app/Http/Controllers/staticPagesController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class staticPagesController extends Controller
+class StaticPagesController extends Controller
 {
     public function home()
     {
